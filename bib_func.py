208ca318c6e920a3814e824bd71efcc95007fc11
Eduardo Benavidez <eduardo.benavidez@gmail.com>
2012-11-08 11:28:38 -0800

    Now, function returns a string

--- bib_func.py
+++ bib_func.py
@@ -5,16 +5,16 @@ from invenio.bibformat_engine import BibFormatObject
 from invenio.bibformat_elements import bfe_INSPIRE_bibtex
 
 def main():
-    print 'in main\n'
-    
     inputTeX = open('file.tex')
     lines = inputTeX.readlines()
     inputTeX.close()
     
-    bibtex_service(lines)
+    bibFile = open('file.bib', 'w')
+    bibtxt = bibtex_service(lines)
+    print bibtxt
+    bibFile.close()
 
 def bibtex_service(lines):
-    print 'in bibtex'
     citeString = '\cite{'
     citeStringMatch = r"\\cite\{(.*?)\}"
     references = []
@@ -27,9 +27,9 @@ def bibtex_service(lines):
                     if not x in references:
                         references.append(x) 
   
-    bibFile = open('file.bib', 'w')
     for x in references:
         index = None
+        btxt_str = ''
         if re.search('.*\:\d{4}\w\w\w?',x) : index = 'texkey'
         elif re.search('.*\/\d{7}',x)      : index = 'eprint'
         elif re.search('\d{4}\.\d{4}',x)   : index = 'eprint'
@@ -42,14 +42,13 @@ def bibtex_service(lines):
             recid_list = perform_request_search(p=x)
             if recid_list:
                 bfo = BibFormatObject(recid_list[0])
-                btxt_str = bfe_INSPIRE_bibtex.format_element(bfo)
-                print btxt_str
+                btxt_str = btxt_str + bfe_INSPIRE_bibtex.format_element(bfo)
 
         else :
             print '***************************************'
             print '*** I do not know what',x,'is. ***'
             print '***************************************'
-    bibFile.close()
+        return btxt_str
 
 if __name__ == "__main__":
     main()
